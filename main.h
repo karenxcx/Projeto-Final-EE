356eacde5ce9a398ffd7e3bb854d7cb20cf5ad59
karenxcx <91982585+karenxcx@users.noreply.github.com>
2025-02-14 16:51:30 -0300

    Update main.h

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
  */
 /*/PINOS 
  
- RA0 - Lim - Limite da válvula;        RB0 - Echo - sensor ultrassom; 
+ RA0 - Lim - Limite da vÃ¡lvula;        RB0 - Echo - sensor ultrassom; 
  RA1 - SM1 - Status Motor;             RB1 - UART_Rx - Bluetooth;
  RA2 - SM2 - Status Motor;             RB2 - UART_Tx - Bluetooth;
  RA3 - SM3 - Status Motor;             RB3 - Vent - aciona a ventoinha;
@@ -16,19 +16,19 @@
  RA7 - LED - apenas um led;            RB7 - ICSPDAT - faz nada; 
  /*/
 
-/*/Dados analógicos
- Sensor óptico TCRT-5000 - 0V: valvula aberta, 5V: valvula fechada
+/*/Dados analÃ³gicos
+ Sensor Ã³ptico TCRT-5000 - 0V: valvula aberta, 5V: valvula fechada
  Sensor ultrassom HC-SR04 - altura da bola
- Sensor temperatura LM35 - compensação para altura da bola
+ Sensor temperatura LM35 - compensaÃ§Ã£o para altura da bola
  * Todos conectados em VDD = 5V; 
- * RA0: INPUT  - Lim - sensor óptico;
+ * RA0: INPUT  - Lim - sensor Ã³ptico;
  * RA6: INPUT  - Echo - sensor ultrassom;
  * RB4: INPUT  - Temp  - sensor de temperatura; 
  * RB0: OUTPUT - Trigger - sensor ultrassom;
  * RA7: OUTPUT - LED;
  /*/
 
-/*/Comunicação Bluetooth
+/*/ComunicaÃ§Ã£o Bluetooth
  * UART
  * RB1 - UART_Rx: 7 bytes;
  * RB2 - UART_Tx: 15 bytes;
@@ -53,19 +53,19 @@
 #include <xc.h> // include processor files - each processor file is guarded.  
 #include <math.h>
 
-/* Medição da Altura da Bola*/
+/* MediÃ§Ã£o da Altura da Bola*/
 
 #define T0      273.15       // [K]   - Temperatura em Kelvin a 0C;
 #define C0      331.3        // [m/s] - Velocidade do som a 0C; 
-#define COEF    0.0488       // Coeficiente para conversão para 0C = 50/1023
+#define COEF    0.0488       // Coeficiente para conversÃ£o para 0C = 50/1023
 
-/* Variáveis de controle PI*/
+/* VariÃ¡veis de controle PI*/
 
 #define ganhoK  0.1          // Ganho proporcional
-#define tempoK  1.5          // Tempo de interação / Tempo de ação integral
+#define tempoK  1.5          // Tempo de interaÃ§Ã£o / Tempo de aÃ§Ã£o integral
 #define ganhoD  0.05         // Ganho derivativo
 
-/* Modos de operação */
+/* Modos de operaÃ§Ã£o */
 
 #define MANUAL 0
 #define VENTOINHA 1
@@ -78,8 +78,8 @@
 #define MAX_PASSOS 420
 
 
-/* Macro para limtar o valor das variáveis de pwm e valvula
-A macro é utilizada para limitar um valor dentro de um intervalo mínimo e máximo.
+/* Macro para limtar o valor das variÃ¡veis de pwm e valvula
+A macro Ã© utilizada para limitar um valor dentro de um intervalo mÃ­nimo e mÃ¡ximo.
 Ela impede que um valor ultrapasse um limite superior ou fique abaixo de um limite inferior.
 
 Se valor for menor que min, retorna min.
@@ -89,14 +89,14 @@ Se valor estiver dentro do intervalo, retorna valor normalmente.
 
 #define Limites(valor, min, max) ((valor) < (min) ? (min) : ((valor) > (max) ? (max) : (valor)))
 
-float temperature = 0, temperature_kelvin = 0, time_of_flight = 0, soundspeed = 0, valvula =0;
+uint16_t temperature = 0, temperature_kelvin = 0, time_of_flight = 0, soundspeed = 0, valvula =0;
 uint8_t temperature_index = 0, rxbuffer[7];
 uint16_t soundspeed_t;
 
 float  valvula_desejada = 0, erroAcumulado = 0, erroAnterior = 0;
 //bool Alterar_PWM, enviado = false;
-//uint8_t Modo_Operacao;      // Recebe o modo de operação pela comunicação
-uint16_t pwm;               // Ciclo útil do motor (entre 0 e 1023)
+//uint8_t Modo_Operacao;      // Recebe o modo de operaÃ§Ã£o pela comunicaÃ§Ã£o
+uint16_t pwm;               // Ciclo Ãºtil do motor (entre 0 e 1023)
 
 uint16_t passo_atual = 0;
 bool valvula_aberta = false;
@@ -121,10 +121,10 @@ typedef struct {
     U16_Union pwm;
 } DADOS;
 
-// Declaração da variável global de dados
+// DeclaraÃ§Ã£o da variÃ¡vel global de dados
 DADOS dados;
 
-// Inicialização das funções
+// InicializaÃ§Ã£o das funÃ§Ãµes
 void triggerUltrasonic();
 float calculateHeight();
 void calculateToF();
@@ -141,7 +141,7 @@ void AbreValvula();
 void FechaValvula();
 
 
-/*/Velocidade do som em [m/s] para temperaturas de 0ºC a 50ºC */
+/*/Velocidade do som em [m/s] para temperaturas de 0ÂºC a 50ÂºC */
 const float LUTsoundspeed[51] = {
     331.3,331.9,332.5,333.1,333.7,334.3,334.9,335.5,336.1,336.7,
     337.3,337.9,338.5,339.1,339.7,340.3,340.9,341.5,342.0,342.6,
